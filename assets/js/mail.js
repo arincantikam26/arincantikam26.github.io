46018f3f8af675b93b7703e9c845e149338114fd
Arin Cantika <arincantikam@gmail.com>
2023-05-24 09:12:43 +0700

    repair contact

---
+++ assets/js/mail.js
@@ -0,0 +1,33 @@
+function sendMail() {
+    var name_val = document.getElementById("name");
+    var email_val = document.getElementById("email");
+    var msg_val = document.getElementById("message");
+    
+    if (name_val.value == "" | email_val.value == "" | msg_val.value == "") {
+        alert('Please fill the form.')
+    } else {
+        var params = {
+            to_name: "arincantikam@gmail.com",
+            name: name_val.value,
+            email: email_val.value,
+            message: msg_val.value,
+        };
+      
+        const serviceID = "service_6ickh6s";
+        const templateID = "acm26github_gzwvzui";
+      
+        emailjs.send(serviceID, templateID, params)
+        .then(res=>{
+            name_val.value = "";
+            email_val.value = "";
+            msg_val.value = "";
+            console.log(res);
+            alert("Your message sent successfully!!")
+    
+        })
+          .catch(err=>console.log(err));
+    }
+  
+  }
+  
+  
